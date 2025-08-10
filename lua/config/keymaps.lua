bf389d272b43cb8387c7788425542c6d2327abb5
delt4d <leandrocruz717@outlook.com>
2025-08-10 10:49:02 -0400

    chore: little fix on open terminal keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,1 @@
-if vim.g.vscode then
-    vim.keymap.set({ 'n', 'i' }, '<C-j>', '<ESC>:terminal<CR>i', { noremap = true, silent = true })
-end
+vim.keymap.set({ 'n', 'i' }, '<C-j>', '<ESC>:terminal<CR>i', { noremap = true, silent = true })
