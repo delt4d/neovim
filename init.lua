44b71334749ded1ff8809884d2f70f4d86226d66
delt4d <leandrocruz717@outlook.com>
2025-08-09 22:18:31 -0400

    chore: init vscode configuration

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 require("config.options")
 require("config.lazy")
+
+if vim.g.vscode then
+   require("config.vscode")
+end
---
+++ lua/config/vscode.lua
@@ -0,0 +1,14 @@
+local vscode = require("vscode")
+
+vim.notify = vscode.notify
+vim.g.clipboard = vim.g.vscode_clipboard
+
+vim.keymap.set({"n", "x"}, "F2", function() 
+   vscode.with_insert(function() 
+      vscode.action("editor.action.refactor")
+   end)
+end)
+
+vim.keymap.set("n", "C-n", function() 
+   vscode.call("explorer.openToSide")
+end)
