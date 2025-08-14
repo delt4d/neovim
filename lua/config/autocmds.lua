a0d73e62fafefe70ef4266cbd231c7e92bafbfc7
delt4d <leandrocruz717@outlook.com>
2025-08-14 17:19:28 -0400

    chore: change .razor to be treated as .cs

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,5 +1,5 @@
--- Treat .razor files as html
+-- Treat .razor files as csharp code
 vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     pattern = "*.razor",
-    command = "set filetype=html"
+    command = "set filetype=cs"
 })
