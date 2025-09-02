746690c14d78fb8a14e49be1233448728815dfbd
delt4d <leandrocruz717@outlook.com>
2025-09-02 15:23:26 -0400

    chore: create rule for three quotes in nvim-autopairs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,5 +1,24 @@
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true
+    config = function ()
+        local npairs = require('nvim-autopairs')
+        local Rule = require('nvim-autopairs.rule')
+
+        npairs.setup({})
+
+        npairs.add_rules({
+            Rule("'''", "'''")
+                :with_pair(function() return true end)
+                :with_move(function() return false end)
+                :with_del(function() return true end)
+                :with_cr(function() return true end),
+
+            Rule('"""', '"""')
+                :with_pair(function() return true end)
+                :with_move(function() return false end)
+                :with_del(function() return true end)
+                :with_cr(function() return true end),
+        })
+    end
 }
