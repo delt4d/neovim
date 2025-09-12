a3f1f45d7b135cc29cbac12a6cd31fe67dd827de
delt4d <leandrocruz717@outlook.com>
2025-09-12 15:15:53 -0400

    chore: create picker for themes

--- lua/shared/theme.lua
+++ lua/shared/theme.lua
@@ -49,7 +49,6 @@ local function apply(theme, bg_opt)
     o.background = bg_opt
     last_opt = bg_opt
     save_config()
-    vim.notify("Switched to theme " .. theme, vim.log.levels.INFO)
     return true
 end
 
@@ -61,18 +60,92 @@ function M.UseLight()
     apply(light_theme, "light")
 end
 
+local function pick_theme_and_set(target)
+    -- Save current theme to restore if user cancels
+    local prev_theme = vim.g.colors_name or ""
+    local prev_bg = o.background
+
+    local themes = vim.fn.getcompletion("", "color")
+    local pickers = require("telescope.pickers")
+    local finders = require("telescope.finders")
+    local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
+    local conf = require("telescope.config").values
+
+    pickers.new({}, {
+        prompt_title = "Select " .. target .. " theme",
+        finder = finders.new_table(themes),
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+            local function set_selected()
+                local selection = action_state.get_selected_entry()
+                local theme = selection[1]
+                if target == "dark" then
+                    dark_theme = theme
+                    apply(theme, "dark")
+                else
+                    light_theme = theme
+                    apply(theme, "light")
+                end
+                save_config()
+                M.RefreshCommands()
+                vim.notify(target:gsub("^%l", string.upper) .. " theme set to " .. theme, vim.log.levels.INFO)
+                actions.close(prompt_bufnr)
+            end
+
+            actions.select_default:replace(set_selected)
+
+            map("i", "<ESC>", function()
+                -- Restore previous theme if cancelled
+                pcall(cmd.colorscheme, prev_theme)
+                o.background = prev_bg
+                actions.close(prompt_bufnr)
+            end)
+
+            map("n", "<ESC>", function()
+                pcall(cmd.colorscheme, prev_theme)
+                o.background = prev_bg
+                actions.close(prompt_bufnr)
+            end)
+
+            -- Live preview on move
+            map("i", "<C-j>", function()
+                actions.move_selection_next(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                if selection then pcall(cmd.colorscheme, selection[1]) end
+            end)
+
+            map("i", "<C-k>", function()
+                actions.move_selection_previous(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                if selection then pcall(cmd.colorscheme, selection[1]) end
+            end)
+
+            return true
+        end,
+    }):find()
+end
+
 function M.SetDark(theme)
-    dark_theme = theme
-    save_config()
-    M.RefreshCommands()
-    vim.notify("Dark theme set to " .. theme, vim.log.levels.INFO)
+    if theme and theme ~= "" then
+        dark_theme = theme
+        save_config()
+        M.RefreshCommands()
+        vim.notify("Dark theme set to " .. theme, vim.log.levels.INFO)
+    else
+        pick_theme_and_set("dark")
+    end
 end
 
 function M.SetLight(theme)
-    light_theme = theme
-    save_config()
-    M.RefreshCommands()
-    vim.notify("Light theme set to " .. theme, vim.log.levels.INFO)
+    if theme and theme ~= "" then
+        light_theme = theme
+        save_config()
+        M.RefreshCommands()
+        vim.notify("Light theme set to " .. theme, vim.log.levels.INFO)
+    else
+        pick_theme_and_set("light")
+    end
 end
 
 function M.Toggle()
@@ -96,7 +169,7 @@ function M.RefreshCommands()
     api.nvim_create_user_command("SetDarkTheme", function(opts)
         M.SetDark(opts.args)
     end, {
-        nargs = 1,
+        nargs = "?",
         complete = "color",
         desc = "Set which theme should be used as dark (current: " .. (dark_theme or "not set") .. ")" 
     })
@@ -104,7 +177,7 @@ function M.RefreshCommands()
     api.nvim_create_user_command("SetLightTheme", function(opts)
         M.SetLight(opts.args)
     end, {
-        nargs = 1,
+        nargs = "?",
         complete = "color",
         desc = "Set which theme should be used as light (current: " .. (light_theme or "not set") .. ")" 
     })
