d3af513589185f20752dcaa3cd05b990fafbafd8
delt4d <leandrocruz717@outlook.com>
2025-10-12 17:05:32 -0400

    chore: add neotree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,39 @@
+return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons"
+        },
+        lazy = false, -- neo-tree will lazily load itself
+        config = function()
+            require("neo-tree").setup({
+                open_files_do_not_replace_types = { "Trouble", "qf", "edgy" },
+            })
+
+            -- Toggle the tree
+            vim.keymap.set('n', '<C-n>', ':Neotree toggle filesystem reveal float<CR>')
+        end
+    },
+    {
+        "s1n7ax/nvim-window-picker",
+        version = "2.*",
+        config = function()
+            require("window-picker").setup({
+                filter_rules = {
+                    include_current_win = false,
+                    autoselect_one = true,
+                    -- filter using buffer options
+                    bo = {
+                        -- if the file type is one of following, the window will be ignored
+                        filetype = { "neo-tree", "neo-tree-popup", "notify" },
+                        -- if the buffer type is one of following, the window will be ignored
+                        buftype = { "terminal", "quickfix" },
+                    },
+                },
+            })
+        end,
+    },
+}
